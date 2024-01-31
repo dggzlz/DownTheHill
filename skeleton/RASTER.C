009b72a0428924b4704b025b032d2922643c6781
DiegoMRU <dgonz348@mtroyal.ca>
2024-01-30 17:45:55 -0700

    Updated the header file and c file for raster. these are the prototypes I believe we need.

--- skeleton/RASTER.C
+++ skeleton/RASTER.C
@@ -1,6 +1,39 @@
 #include "raster.h"
 
-void plot_something_1(UINT16 *base, int x, int y, ...)
+typedef unsigned long long UINT64;
+
+void clear_screen()
 {
-	/* [TO DO] delete this example function */
+
+}
+
+void plot_pixel(char *base, int x, int y)
+{
+	if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT)
+		*(base + y * 80 + (x >> 3)) |= 1 << 7 - (x & 7);
+}
+
+void plot_vline()
+{
+
+}
+
+void plot_hline()
+{
+
+}
+
+void plot_line()
+{
+
+}
+
+
+
+void plot_bitmap_64(UINT64 *base, 
+					int x, int y,
+					const UINT64 *bitmap,
+					unsigned int height)
+{
+
 }
