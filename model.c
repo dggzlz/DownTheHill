defd630fba8bfb161a15ca5256e7f6f3f717c1c1
Juan Diego Serrato <jserr894@mtroyal.ca>
2024-02-11 02:44:41 -0700

    added model.c to include behaviour functions of our objects

---
+++ model.c
@@ -0,0 +1,39 @@
+#include model.h
+
+/*behaviour functions to add:
+
+********************************snowBoarder
+- moveSnowboarder
+- snowBoarderCollidesSkier
+- snowBoarderCollidesObstacle
+
+********************************skier
+- moveSkier
+- snowBoarderCollidesSkier
+- skierCollidesObstacles
+- skierSpawns
+- skierDissapears like my fourth wife
+
+*********************************scoreCounter
+-scoreIncrease
+
+*********************************finalScore
+- calculateFinalScore //in game score + (#skiersHit * 3)//
+
+
+*********************************skiersHit
+- countIncrease
+
+*********************************tree
+- moveTree
+- spawnTree
+
+
+
+
+
+
+
+
+
+*/
