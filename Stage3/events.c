4311b56281d80e6ecda3d7f9f530f6a1d83d96e3
DiegoMRU <dgonz348@mtroyal.ca>
2024-02-15 12:29:43 -0700

    updating events files

--- Stage3/events.c
+++ Stage3/events.c
@@ -1,2 +1,41 @@
 #include "events.h"
 
+/****ANSYNCHRONOUS EVENTS****/
+
+void moveRequest(){
+    char  = Cconin();
+
+    if (ch == 0x1B){ /*ASCII code = left arrow*/
+        moveSnowboarder(player, )
+    }
+    else if (ch == )
+}
+void quit();        /*triggered by the key Esc from keyboard*/
+
+/****SYNCHRONOUS TIMED EVENTS*****/
+
+void skierSpawn(); /*every 5 seconds*/
+void skierMoves(); /*every 2 seconds*/
+void skierMovesUp(); /*every 2 seconds*/
+void treeSpawn(); /*every second*/
+void treeMoves(); /*every 0.5 seconds*/
+
+/*****CONDITION-BASED EVENTS*****/
+
+/*Collisions*/
+void obsCollision(); /*Obstacle collision event triggered by the player hitting the obstacle*/
+void edgeCollision();/* triggered when the player touches either vertical edge*/
+void topCollision(); /* triggered when the obstcale or the skier hits the top edge*/
+
+/*position reset*/
+void invincible(); /*triggered by the Obstacle collision or the edge collision*/
+void blink(); /*triggered by the Obstacle collision or the edge collision*/
+void decreaseLife(); 
+void backToCentre();
+
+/*Skier Collision*/
+void skierCollision();
+void increaseCounter();
+
+/*Game ends*/
+void gameOver();
--- Stage3/events.h
+++ Stage3/events.h
@@ -8,31 +8,31 @@ void quit();        /*triggered by the key Esc from keyboard*/
 
 /****SYNCHRONOUS TIMED EVENTS*****/
 
-skierSpawn(); /*every 5 seconds*/
-skierMoves(); /*every 2 seconds*/
-skierMovesUp(); /*every 2 seconds*/
-treeSpawn(); /*every second*/
-treeMoves(); /*every 0.5 seconds*/
+void skierSpawn(); /*every 5 seconds*/
+void skierMoves(); /*every 2 seconds*/
+void skierMovesUp(); /*every 2 seconds*/
+void treeSpawn(); /*every second*/
+void treeMoves(); /*every 0.5 seconds*/
 
 /*****CONDITION-BASED EVENTS*****/
 
 /*Collisions*/
-obsCollision(); /*Obstacle collision event triggered by the player hitting the obstacle*/
-edgeCollision();/* triggered when the player touches either vertical edge*/
-topCollision(); /* triggered when the obstcale or the skier hits the top edge*/
+void obsCollision(); /*Obstacle collision event triggered by the player hitting the obstacle*/
+void edgeCollision();/* triggered when the player touches either vertical edge*/
+void topCollision(); /* triggered when the obstcale or the skier hits the top edge*/
 
 /*position reset*/
-invincible(); /*triggered by the Obstacle collision or the edge collision*/
-blink(); /*triggered by the Obstacle collision or the edge collision*/
-decreaseLife(); 
-backToCentre();
+void invincible(); /*triggered by the Obstacle collision or the edge collision*/
+void blink(); /*triggered by the Obstacle collision or the edge collision*/
+void decreaseLife(); 
+void backToCentre();
 
 /*Skier Collision*/
-skierCollision();
-increaseCounter();
+void skierCollision();
+void increaseCounter();
 
 /*Game ends*/
-gameOver();
+void gameOver();
 
 
 #endif
