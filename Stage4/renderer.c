fdab0789c75a8bb912931a71b2d0f2283e43d154
Juan Diego Serrato <jserr894@mtroyal.ca>
2024-03-13 15:01:16 -0600

    stage4 changes

--- Stage4/renderer.c
+++ Stage4/renderer.c
@@ -10,16 +10,21 @@
 
 
 
-void renderPlayer(Snowboarder *player, UINT32 *base32)
+void renderPlayerRight(Snowboarder *player, UINT32 *base32)
 {
-    plotBitmap64(base32, player->x, 100, snowboarderBM, 128);
+    plotBitmap64(base32, player->x, 100, RightSnowBoarder, 128);
+}
+
+void renderPlayerLeft(Snowboarder *player, UINT32 *base32)
+{
+    plotBitmap64(base32, player->x, 100, LeftSnowBoarder, 128);
 }
 
 void renderFakePlayer(UINT32 *base32) /*object for 5 second invincibility*/
 {
     int x = 320;
     int y = 100;
-    plotBitmap64(base32, x, y, snowboarderBM, 128);
+    plotBitmap64(base32, x, y, RightSnowBoarder, 128);
 }
 
 void renderSkier(NPCskier *skier, UINT32 *base32)
@@ -34,21 +39,38 @@ void renderTree(Tree *tree, UINT32 *base32)
     /*printf("tree located at x: %d, y: %d\n", tree->x,tree->y);*/
 }
 
-
-
 void renderLives(Lives *lives, UINT32 *base32)
 {
     int count = lives->numLives;
-    /*printf("number of lives is: %d\n", lives->numLives);
-    printf("hearts displayed is: %d\n",count)
-    */int xCoor = lives->x;
+    int xCoor = lives->x;
 
     while(count>0)
     {
         plotBitmap32(base32, xCoor,lives->y, heartLifeBM, 32);
-        /*printf("location of life is: %d\n", xCoor);*/
         xCoor -= 32;
         count--;
     }
 
+}
+
+renderModel(const Model *model, UINT32 *base32)
+{
+    int i;
+    if(!collisionCheck)
+    {
+        if (key == left)
+        {
+            renderPlayerLeft(model->player, base32);
+        }
+        else if (key == right)
+        {
+            renderPlayerRight(model->player, base32);
+        }
+    }
+    else
+    renderFakePlayer(base32);
+    for(i = 0; i < treeArray.size; i++)
+    {
+        renderTree(model->tree, base32);
+    }
 }
